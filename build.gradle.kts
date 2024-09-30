6788f6ad1a05409d5dfa6b1cd8a07a611ed15997
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-09-30 14:45:06 +0100

    reorganise modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,20 +3,12 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":shared"))
-    implementation(project(":wire"))
-    testImplementation(project(":test"))
-    runtimeOnly("org.liquibase:liquibase-core")
-    runtimeOnly("com.h2database:h2")
 }
 
 testing {
     suites {
         val integrationTest by getting(JvmTestSuite::class) {
             dependencies {
-                implementation(project(":shared"))
-                implementation(project(":wire"))
-                implementation(project(":test"))
             }
         }
     }
